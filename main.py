fd7e84673973ffe6a93716cfc4a10dd10c242987
ChiragSharma-08 <257071564+ChiragSharma-08@users.noreply.github.com>
2026-05-07 02:16:21 +0530

    Class Variables
    In this topic we have learned about class variables by making a few class variables and modifying some

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from car import Car
+#class variable = shared among all instances of a class
+#                 Defined outside a constructor(construct = def __init__ in easy words)
+#                 Allow you to share data among all objects created from that class
 
-car1 = Car("Mustang", 2024, "red", False)
-car2 = Car("Corvette", 2025, "blue", True)
+class Student:
+
+    class_year = 2026
+    num_students = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.num_students += 1
+
+student1 = Student("Spongebob", 30)
+student2 = Student("Peter", 40)
+student3 = Student("Pankaj", 50)
 
 
-print(car2.model)
-print(car2.year)
-print(car2.color)
-print(car2.forsale)
+print(student1.name)
+print(student1.age)
+print(student1.class_year)
+print(Student.class_year)
 
-car1.drive()
-car1.stop()
-car1.describe()
+print(Student.num_students)
+
+print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
